049d3b0b57d83079e778c63cef6c13899d9c1ab9
Brian McEntee <brian.m.mcentee@gmail.com>
2015-11-11 23:15:10 -0500

    starting helper class CGTools for use with points, vertices, and polygons

---
+++ CGTools.java
@@ -0,0 +1,17 @@
+package compgeo_lib;
+/*##############################################################################
+	Class CGTools -- a helper class for use with Points, Vertexes, and Polys
+
+	Area(Point2D a, Point2D b, Point2D c) -- returns area of triangle represented
+	
+	Left(Point2D a, Point2D b, Point2D c) -- determines if c is left of a-b
+
+	LeftOn(Point2D a, Point2D b, Point2D c) --	determines if c is left of or
+	collinear with a-b
+
+	Collinear(Point2D a, Point2D b, Point2D c) -- determines if c is collinear
+	with a-b
+
+	@author Brian McEntee
+	@version 1.0
+##############################################################################*/
